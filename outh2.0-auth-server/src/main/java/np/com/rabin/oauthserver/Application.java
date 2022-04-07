bc4b0bcad050794e43827407faa57e1230bd6d54
Rabin Awal <morbid_leathal@hotmail.com>
2022-04-07 11:20:41 -0500

    Proposed OAuth implementation and Added OAuth2.0 server module

---
+++ outh2.0-auth-server/src/main/java/np/com/rabin/oauthserver/Application.java
@@ -0,0 +1,13 @@
+package np.com.rabin.oauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
