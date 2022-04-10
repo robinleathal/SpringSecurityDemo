9c5ecd1cfdf075be7f757849026e1b21dd3dce6d
Rabin Awal <morbid_leathal@hotmail.com>
2022-04-10 00:24:03 -0500

    Added resource server module with ResourceServerConfig Client module with WebClientConfiguration...

---
+++ oauth-resource-server/src/main/java/np/com/rabin/oauthresourceserver/controller/UserController.java
@@ -0,0 +1,13 @@
+package np.com.rabin.oauthresourceserver.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/api/users")
+    public String[] getUser() {
+        return new String[]{"Rabin", "Kabin","Mona"};
+    }
+}
